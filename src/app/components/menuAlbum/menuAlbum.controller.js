4efff5fb0f897680bcad6aa4b63bf4034bdd5c9d
Aurélie Borgne <aurelie.borgne@1000mercis.com>
2016-08-22 18:41:09 +0200

    menu album

--- src/app/components/menuAlbum/menuAlbum.controller.js
+++ src/app/components/menuAlbum/menuAlbum.controller.js
@@ -10,11 +10,15 @@
     var menuAlbum = this;
 
     var cities = Cities.data;
+    var slider = $('#slider');
+
 
     menuAlbum.selectCity = cities[($rootScope.idSelectCity-1)].title;
 
     menuAlbum.photos = Photos.getPhotosCity($rootScope.idSelectCity);
     $rootScope.idSelectPhoto = 0;
+
+    var maxPhotos = menuAlbum.photos.nbImg;
 
     menuAlbum.toggleMenu = function(){
       menuAlbum.isOpen = !menuAlbum.isOpen;
@@ -32,6 +36,22 @@
     menuAlbum.selectPhoto = function(idSelectPhoto){
       $rootScope.idSelectPhoto = idSelectPhoto;
       $rootScope.$broadcast('updateIdSelectPhoto');
+    }
+
+    menuAlbum.prevSlide = function(){
+      var lastSlide = $('#slider li:last-child'); 
+      TweenMax.to(slider, 0.25,{left:0, ease:Sine.easeOut, onComplete:function(){
+        slider.prepend(lastSlide);
+        slider.css('left', '-193px');
+      }});
+    }
+
+    menuAlbum.nextSlide = function(){
+      var firstSlide = $('#slider li:first-child'); 
+      TweenMax.to(slider, 0.25,{left:'-386px', ease:Sine.easeOut, onComplete:function(){
+        slider.append(firstSlide);
+        slider.css('left', '-193px');
+      }});      
     }
 
 
